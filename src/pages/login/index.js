9eef3f42fe2a355e6e397e2ca32ce7bc17f26d17
Matheus Benevenuto <matthewbene03@gmail.com>
2026-01-27 15:28:16 -0300

    Limpando o projeto react e começando a configurações estilização para as paginas. Criando arquivo de cores, menu da aplicação.

---
+++ src/pages/login/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Title } from "./styled"
+import { Container } from "../../style/GlobalStyles"
+
+
+function Login() {
+  return (
+    <Container>
+      <Title>Login</Title>
+    </Container>
+  );
+}
+
+export default Login;
